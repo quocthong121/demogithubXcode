02deba9a860439f536fac4544e9a746a55f06c97
quocthong121 <20211tt1072@mail.tdc.edu.vn>
2022-05-27 02:13:02 -0700

    Initial Commit

---
+++ thongdemogithub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  thongdemogithub
+//
+//  Created by thong on 5/27/22.
+//  Copyright © 2022 thong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
